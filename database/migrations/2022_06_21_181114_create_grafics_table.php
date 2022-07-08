634585b38676a9ede3ec6012eb5fd7c439375002
bartp <bartek.paraf@gmail.com>
2022-07-08 16:11:46 +0200

    reformat to psr12

--- database/migrations/2022_06_21_181114_create_grafics_table.php
+++ database/migrations/2022_06_21_181114_create_grafics_table.php
@@ -4,32 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-  /**
-   * Run the migrations.
-   *
-   * @return void
-   */
-  public function up()
-  {
-    Schema::create('grafics', function (Blueprint $table) {
-      $table->id();
-      $table->string('name');
-      $table->string('file');
-      $table->string('type');
-      $table->string('user_id');
-      $table->timestamps();
-    });
-  }
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('grafics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('file');
+            $table->string('type');
+            $table->string('user_id');
+            $table->timestamps();
+        });
+    }
 
-  /**
-   * Reverse the migrations.
-   *
-   * @return void
-   */
-  public function down()
-  {
-    Schema::dropIfExists('grafics');
-  }
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('grafics');
+    }
 };
