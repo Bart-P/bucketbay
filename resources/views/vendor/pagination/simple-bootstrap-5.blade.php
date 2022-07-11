800d3bb39bff25d2464b20e60bbeed84e9d2d416
bartp <bartek.paraf@gmail.com>
2022-07-11 21:10:06 +0200

    refactor + added todo

--- resources/views/vendor/pagination/simple-bootstrap-5.blade.php
+++ resources/views/vendor/pagination/simple-bootstrap-5.blade.php
@@ -7,6 +7,7 @@
                     <span class="page-link">{!! __('pagination.previous') !!}</span>
                 </li>
             @else
+                <!-- TODO change links to buttons with wire:click as per documantation, check if page resets if you are on page 2-3 and change items per page -->
                 <li class="page-item">
                     <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev">
                         {!! __('pagination.previous') !!}
@@ -17,7 +18,8 @@
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
                 <li class="page-item">
-                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next">{!! __('pagination.next') !!}</a>
+                    <a class="page-link" href="{{ $paginator->nextPageUrl() }}"
+                        rel="next">{!! __('pagination.next') !!}</a>
                 </li>
             @else
                 <li class="page-item disabled" aria-disabled="true">
