82c6a22616c73bb5d4b8a1f5170f8b79dd713501
bartp <bartek.paraf@gmail.com>
2022-07-13 21:03:24 +0200

    added auth middleware group and login/logout/authenticate routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\GraficController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,11 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [ItemController::class, 'index']);
-Route::get('/login', fn () => view('login'));
+Route::get('/login', [UserController::class, 'login'])->name('login');
 
-Route::get('/grafics', [GraficController::class, 'grafics']);
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 
-Route::resources([
-    'addresses' => AddressController::class,
-]);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [ItemController::class, 'index']);
+
+    Route::post('/logout', [UserController::class, 'logout']);
+
+    Route::get('/grafics', [GraficController::class, 'grafics']);
+
+    Route::resources([
+        'addresses' => AddressController::class,
+    ]);
+});
