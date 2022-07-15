9ce512c62516446f8306e9cce4eeb590c5295779
bartp <bartek.paraf@gmail.com>
2022-07-15 20:37:10 +0200

    reformatted to psr12

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -18,16 +18,16 @@ class AddressFactory extends Factory
     public function definition()
     {
         return [
-      'name1' => $this->faker->name(),
-      'name2' => $this->faker->text(10),
-      'name3' => $this->faker->text(5),
-      'street' => $this->faker->streetAddress(),
-      'street_nr' => $this->faker->buildingNumber(),
-      'city' => $this->faker->city(),
-      'city_code' => $this->faker->randomNumber(5, true),
-      'country' => $this->faker->country(),
-      'address_info' => $this->faker->text(15),
-      'user_id' => User::all()->random()->id,
-    ];
+            'name1' => $this->faker->name(),
+            'name2' => $this->faker->text(10),
+            'name3' => $this->faker->text(5),
+            'street' => $this->faker->streetAddress(),
+            'street_nr' => $this->faker->buildingNumber(),
+            'city' => $this->faker->city(),
+            'city_code' => $this->faker->randomNumber(5, true),
+            'country' => $this->faker->country(),
+            'address_info' => $this->faker->text(15),
+            'user_id' => User::all()->random()->id,
+        ];
     }
 }
