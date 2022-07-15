0cdd3a231c00761af8f14aab4afd6f105d93fbe0
bartp <bartek.paraf@gmail.com>
2022-07-15 20:36:24 +0200

    added hasMany grafics relationship to User

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,4 +32,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Address::class);
     }
+
+    public function grafics()
+    {
+        return $this->hasMany(Grafic::class);
+    }
 }
