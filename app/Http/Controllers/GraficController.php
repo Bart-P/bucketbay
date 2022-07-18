0411081b64bff3f1fb364f3164e17c330f693fb2
bartp <bartek.paraf@gmail.com>
2022-07-18 21:16:27 +0200

    added store method

--- app/Http/Controllers/GraficController.php
+++ app/Http/Controllers/GraficController.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Grafic;
+use Illuminate\Validation\Rule;
+
 class GraficController extends Controller
 {
     public function grafics()
     {
         return view('grafics.index');
+    }
+
+    public function store()
+    {
+        // TODO symlink folder to public
+        // TODO create the preview for grafics
+        $fileName = request()->file('file')->store('grafics');
+        if ($fileName && auth()->user()->id) {
+            $form_fields = request()->validate([
+                'name' => ['required', Rule::unique('grafics', 'name')],
+            ]);
+
+            $form_fields['user_id'] = auth()->user()->id;
+            $form_fields['file'] = $fileName;
+
+            Grafic::create($form_fields);
+
+            return redirect('/grafics')->with('success_msg', 'Datei wurde hochgeladen');
+        };
     }
 }
