007ca3f572bd396683106ade51ccdd953507d1d7
bartp <bartek.paraf@gmail.com>
2022-08-22 21:10:40 +0200

    minor refactor

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,18 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Address;
 use App\Models\Grafic;
+use Illuminate\Contracts\View\View;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Psr\Container\NotFoundExceptionInterface;
+use Psr\Container\ContainerExceptionInterface;
 
 class CartController extends Controller
 {
+    /**
+     * @return View|Factory
+     * @throws BindingResolutionException
+     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface
+     */
     public function index()
     {
         $currentAddress = Address::find(session('shopping-cart.delivery-address-id'));
         $currentGrafics = [];
-        if (session('shopping-cart.grafics')) {
-            foreach (session('shopping-cart.grafics') as $graficsId) {
+        if (session('shopping-cart.grafics-id')) {
+            foreach (session('shopping-cart.grafics-id') as $graficsId) {
                 array_push($currentGrafics, Grafic::find($graficsId));
             };
         }
+
         return view('cart.index', [
             'address' => $currentAddress,
             'grafics' => $currentGrafics,
