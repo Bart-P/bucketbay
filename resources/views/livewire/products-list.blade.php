2e4a6dbd9b865a5f994c444d3a80c2dad99fdb20
bartp <bartek.paraf@gmail.com>
2022-08-24 20:58:42 +0200

    added button to remove one from cart as well as text how many of each product are currently in cart

--- resources/views/livewire/products-list.blade.php
+++ resources/views/livewire/products-list.blade.php
@@ -1,3 +1,4 @@
+@inject('cartService', 'App\Services\CartService')
 <div class="row g-3">
     @foreach ($items as $item)
         <div class="col">
@@ -6,18 +7,33 @@
                     <img src="{{ asset('/images/items/' . $item['image']) }}" class="card-img-top p-4"
                          alt="Bild vom Eimer">
                 </div>
+
                 <div class="card-title text-center pt-4 fw-bold text-uppercase">
                     {{ $item['name'] }}
                 </div>
 
                 <div class="card-body ">
+
                     <p class="card-text">
                         {{ $item['description'] }}
                     </p>
 
-                    <button wire:click="addProductToCart({{$item->id}})" type="button" class="btn btn-outline-success float-end">
-                        <i class="bi-basket3-fill"></i>
-                    </button>
+                    <div class="{{ $cartService->getQuantity($item->id) > 0 ? 'd-flex justify-content-between' : ''}}">
+
+                        @if($cartService->getQuantity($item->id))
+                            <button wire:click="removeOneProductFromCart({{$item->id}})" type="button" class="btn btn-danger">
+                                <i class="bi-dash-circle-fill"></i>
+                            </button>
+
+                            <span class="my-auto">
+                                <b> {{$cartService->getQuantity($item->id)}} Stück im Warenkorb</b>
+                            </span>
+                        @endif
+
+                        <button wire:click="addOneProductToCart({{$item->id}})" type="button" class="btn {{ $cartService->getQuantity($item->id) > 0 ? 'btn-success' : 'btn-outline-success float-end'}}">
+                            <i class="bi-basket3-fill"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
