52b2e1aa73abc46bd5898408caeb318e4cba934d
bartp <bartek.paraf@gmail.com>
2022-08-26 20:43:13 +0200

    created ProductInCart livewire controller and passed products and amounts to view

--- app/Http/Livewire/ProductsInCart.php
+++ app/Http/Livewire/ProductsInCart.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
+use App\Services\CartService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class ProductsInCart extends Component
 {
-    public function render()
+    private CartService $cartService;
+
+    public function boot(CartService $cartService)
     {
-        return view('livewire.products-in-cart');
+        $this->cartService = $cartService;
+    }
+
+    public function render(): Factory|View|Application
+    {
+        $productsInCart = $this->cartService->getProducts();
+        return view('livewire.products-in-cart', ['products'       => Product::find($productsInCart->keys()),
+                                                  'productsInCart' => $productsInCart]);
     }
 }
