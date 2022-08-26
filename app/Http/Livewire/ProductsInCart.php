f6dbbf1ed323411027b5686e87983828015ee3b0
bartp <bartek.paraf@gmail.com>
2022-08-26 18:28:05 +0200

    created products in cart as livewire component

---
+++ app/Http/Livewire/ProductsInCart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ProductsInCart extends Component
+{
+    public function render()
+    {
+        return view('livewire.products-in-cart');
+    }
+}
