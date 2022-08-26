9ecc7314d655918a39c9f88f8798dc4c618b1130
bartp <bartek.paraf@gmail.com>
2022-08-26 20:43:52 +0200

    created view and connected to cart

--- resources/views/livewire/products-in-cart.blade.php
+++ resources/views/livewire/products-in-cart.blade.php
@@ -1,36 +1,36 @@
 <div class="col-md-6 col-lg-6 order-md-last">
     <h4 class="d-flex justify-content-between align-items-center mb-3">
         <span class="text-primary">Produkte im Warenkorb</span>
-        <span class="badge bg-primary rounded-pill">3</span>
+        <span class="badge bg-primary rounded-pill">{{ collect($productsInCart)->values()->sum() }}</span>
     </h4>
     <ul class="list-group mb-3">
-        <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-                <h6 class="my-0">Eimer mit Halterung</h6>
-                <small class="text-muted">Kurzbeschreibung</small>
-            </div>
-            <span class="text-muted"><span>4</span> Stück</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-                <h6 class="my-0">Eimer einzeln</h6>
-                <small class="text-muted">Kurzbeschreibung</small>
-            </div>
-            <span class="text-muted"><span>4</span> Stück</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-                <h6 class="my-0">Halterung einzeln</h6>
-                <small class="text-muted">Kurzbeschreibung</small>
-            </div>
-            <span class="text-muted"><span>4</span> Stück</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between lh-sm">
-            <div>
-                <h6 class="my-0">Druck</h6>
-                <small class="text-muted">Kurzbeschreibung</small>
-            </div>
-            <span class="text-muted"><span>4</span> Stück</span>
-        </li>
+        @foreach($products as $product)
+            <li class="list-group-item d-flex justify-content-between lh-sm align-items-center">
+                <div>
+                    <h6 class="my-0">{{ $product->name }}</h6>
+                </div>
+                <div x-data="{edit: false}" class="d-flex gap-4 justify-content-center align-items-center">
+                    <span class="input-group">
+                        <label>
+                            <input type="number" value="{{ $productsInCart[$product->id] }}"
+                                   class="form-control text-end" x-bind:disabled="! edit"
+                                   style="min-width: fit-content" min="0">
+                        </label>
+                        <span class="input-group-text">Stück</span>
+                    </span>
+
+                    <span class="d-flex">
+                        <button x-bind:hidden="!edit" x-on:click="edit = false"
+                                class="btn btn-success border-0">
+                            <i class="bi-save"></i></button>
+
+                        <button x-bind:hidden="edit" x-on:click="edit = true"
+                                class="btn btn-outline-primary border-0"><i
+                                    class="bi-pen"></i></button>
+                        <button class="btn btn-outline-danger border-0"><i class="bi-trash"></i></button>
+                    </span>
+                </div>
+            </li>
+        @endforeach
     </ul>
 </div>
