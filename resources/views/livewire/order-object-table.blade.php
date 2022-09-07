bfabe98e7e4383eb7261ec2c77f5c9376b2e74ee
bp <bartek.paraf@gmail.com>
2022-09-07 20:49:14 +0200

    created order object view

--- resources/views/livewire/order-object-table.blade.php
+++ resources/views/livewire/order-object-table.blade.php
@@ -1,44 +1,68 @@
-<ul class="list-group">
+<table class="table table-bordered h-100">
+    <tr class="text-center">
+        <th>Menge</th>
+        <th>Bild</th>
+        <th>Name</th>
+        <th>Grafik1</th>
+        <th>Grafik2</th>
+        <th>Menge Ändern</th>
+        <th></th>
+    </tr>
     @foreach($orderObjects['printable'] as $printable)
-        <li class="list-group-item text-center">
-            <h5>{{ $products->find( $printable['productId'])->name }}
-                <span class="fst-italic text-muted">
-                    ID:
-                    {{ $printable['productId'] }}
-                </span>
-            </h5>
-        </li>
-        <li class="list-group-item text-center">
-            <h6>Grafik Front <span class="fst-italic text-muted">{{ $printable['graficFrontId'] }}</span></h6>
-        </li>
-        <li class="list-group-item d-flex gap-5 justify-content-center align-items-center">
-            <img src="{{ asset('storage/grafics/' . $grafics->find( $printable['graficFrontId'] )->file) }}"
-                 class="img-fluid rounded" style="height: 100px; width: 150px; object-fit: cover;" alt="">
-            <ul class="list-group">
-                <li class="list-group-item">Name: {{ $grafics->find( $printable['graficFrontId'])->name }}</li>
-                <li class="list-group-item">Typ: {{ $grafics->find( $printable['graficFrontId'])->type }}</li>
-            </ul>
-        </li>
-        @if($printable['graficBackId'] != null)
-            <li class="list-group-item text-center">
-                <h6>Grafik Rücken <span class="fst-italic text-muted">{{ $printable['graficBackId'] }}</span></h6>
-            </li>
-            <li class="list-group-item d-flex gap-5 justify-content-center align-items-center">
-                <img src="{{ asset('storage/grafics/' . $grafics->find( $printable['graficBackId'] )->file) }}"
-                     class="img-fluid rounded" style="height: 100px; width: 150px; object-fit: cover;" alt="">
 
-                <ul class="list-group">
-                    <li class="list-group-item">Name: {{ $grafics->find( $printable['graficBackId'])->name }}</li>
-                    <li class="list-group-item">Typ: {{ $grafics->find( $printable['graficBackId'])->type }}</li>
-                </ul>
-            </li>
-        @endif
-        <hr>
+        <tr>
+            <td class="p-3 align-middle text-center">
+                {{ $printable['quantity'] }}x
+            </td>
+            <td>
+                <img src="{{ asset('/images/items/' . $products->find($printable['productId'])->image) }}"
+                     class="img-fluid" style="height: 100px" alt="">
+            </td>
+            <td>
+                <div class="d-flex h-100 align-middle align-items-center">
+                    {{ $products->find($printable['productId'])?->name }}
+                    <span class="ms-2 text-muted fs-6 fst-italic">id: {{ $printable['productId'] }}</span>
+                </div>
+            </td>
+            <td>
+                <div class="d-flex h-100 justify-content-between align-items-center gap-3">
+                    <img src="{{asset('storage/grafics/' . $grafics->find($printable['graficFrontId'])->file)}}"
+                         class="img-fluid rounded" style="height: 50px; width: 75px; object-fit: cover;" alt="">
+                    {{ $grafics->find($printable['graficFrontId'])->name }}
+                    <button class="btn btn-outline-secondary border-0"><i class="bi-x-circle"></i></button>
+                </div>
+            </td>
+            <td class="d-flex h-100 justify-content-between align-items-center">
+                @if( $printable['graficBackId'] !== null )
+                    <img src="{{asset('storage/grafics/' . $grafics->find($printable['graficBackId'])->file)}}"
+                         class="img-fluid rounded" style="height: 50px; width: 75px; object-fit: cover;"
+                         alt="">
+                    {{ $grafics->find($printable['graficBackId'])->name }}
+                    <button class="btn btn-outline-secondary border-0"><i class="bi-x-circle"></i></button>
+                @else
+                    <button class="btn btn-lg btn-outline-primary mx-auto border-0"><i
+                                class="bi-printer"></i>
+                    </button>
+                @endif
+            </td>
+            <td>
+                <div class="d-flex justify-content-end align-items-center gap-2 h-100">
+                    <label>
+                        <input name="productQuantity" type="number"
+                               value={{ $printable['quantity'] }} class="form-control" min=0
+                               style="max-width: 70px">
+                    </label>
+                    <button class="btn btn-outline-success border-0" style="max-width: 50px"><i
+                                class="bi-save"></i>
+                    </button>
+
+                </div>
+            </td>
+            <td class="text-center align-middle">
+                <button class="btn btn-outline-danger border-0">
+                    <i class="bi-trash"></i>
+                </button>
+            </td>
+        </tr>
     @endforeach
-
-    <ul>
-        @foreach($orderObjects['notPrintable'] as $notPrintable)
-            {{ $notPrintable['productId'] }}
-            {{ $products->find( $notPrintable['productId'])->name }}
-        @endforeach
-    </ul>
+</table>
