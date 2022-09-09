b00e6970f7ed1ee05b1761766b257228a95a1db9
bp <bartek.paraf@gmail.com>
2022-09-09 21:25:20 +0200

    removed @method declaration, was not used anywhere

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,9 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * @method static find(\Illuminate\Support\Collection|null $getProducts)
- */
 class Product extends Model
 {
     use HasFactory;
