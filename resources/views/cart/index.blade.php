84fc6544c9cf87c22284d9024875279f96c7f751
bp <bartek.paraf@gmail.com>
2022-09-28 19:33:28 +0200

    removed main from component and moved functionality to x-layout

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -1,23 +1,21 @@
 <x-layout>
-    <main x-data="{'showModal': false}" @keydown.esc="showModal = false" x-cloak>
-        <x-banner>
-            <x-slot:heading> Warenkorb</x-slot:heading>
-            Hier eine kurze beschreibung wie der Warenkorb funktioniert...
-        </x-banner>
-        <div class="container">
-            <div class="row">
-                <x-delivery-address :address="$address"></x-delivery-address>
-                @livewire('products-in-cart')
-            </div>
-            <hr class="my-4">
-            <div class="">
-                <h3 class="text-primary mb-4">Bestellobjekte</h3>
-                @livewire('order-object-table')
-            </div>
-            <hr class="my-4">
-            <div class="d-flex w-100 justify-content-center">
-                <button class="btn btn-primary btn-lg" type="submit">Bestellung auslösen</button>
-            </div>
+    <x-banner>
+        <x-slot:heading> Warenkorb</x-slot:heading>
+        Hier eine kurze beschreibung wie der Warenkorb funktioniert...
+    </x-banner>
+    <div class="container">
+        <div class="row">
+            <x-delivery-address :address="$address"></x-delivery-address>
+            @livewire('products-in-cart')
         </div>
-    </main>
+        <hr class="my-4">
+        <div class="">
+            <h3 class="text-primary mb-4">Bestellobjekte</h3>
+            @livewire('order-object-table')
+        </div>
+        <hr class="my-4">
+        <div class="d-flex w-100 justify-content-center">
+            <button class="btn btn-primary btn-lg" type="submit">Bestellung auslösen</button>
+        </div>
+    </div>
 </x-layout>
