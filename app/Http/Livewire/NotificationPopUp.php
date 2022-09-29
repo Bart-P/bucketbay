92caa4444e99824dea2fee0da6681439d1cd35e7
bp <bartek.paraf@gmail.com>
2022-09-29 21:39:21 +0200

    added todo and safeguards

--- app/Http/Livewire/NotificationPopUp.php
+++ app/Http/Livewire/NotificationPopUp.php
@@ -9,23 +9,33 @@ use Livewire\Component;
 
 class NotificationPopUp extends Component
 {
-    public $listeners = ['notifyUser'];
-    public $showNotification = false;
-    public $notificationMessages = [];
-
-    public function boot()
-    {
-        $this->notificationMessages = session()->has('notificationMessages') ? session('notificationMessages') : [];
-    }
+    public $listeners = ['notifySuccess', 'hideNotification'];
+    public $show = false;
+    public $notificationType;
 
     public function render(): Factory|View|Application
     {
+        // TODO class is not shown properly.. figure out why it is only shown after reload of page, first page load bg of popup stays white
+        if (session()->has('notificationMessage') && !empty(session('notificationMessage')['message']) && !empty(session('notificationMessage')['type'])) {
+            $this->show = true;
+        } else {
+            session()->put('notificationMessage', ['message' => '', 'type' => '']);
+        }
         return view('livewire.notification-pop-up');
     }
 
-    public function notifyUser()
+    public function notifySuccess()
     {
-        $this->showNotification = true;
-        $this->render();
+        if (session()->has('notificationMessage')) {
+            $this->notificationType = 'success';
+            $this->show = true;
+            $this->render();
+        }
+    }
+
+    public function hideNotification()
+    {
+        $this->show = false;
+        session()->forget('notificationMessage');
     }
 }
