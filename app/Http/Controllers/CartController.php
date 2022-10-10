431af8933607b58b24554760f99b0f5a2ae4c046
bp <bartek.paraf@gmail.com>
2022-10-10 20:58:33 +0200

    removed unuser use statements and cleaned up formatting

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,14 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Address;
-use App\Models\Grafic;
 use App\Services\CartService;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Contracts\Container\BindingResolutionException;
-use Psr\Container\NotFoundExceptionInterface;
-use Psr\Container\ContainerExceptionInterface;
+use Illuminate\Contracts\View\View;
 
 class CartController extends Controller
 {
@@ -24,9 +20,6 @@ class CartController extends Controller
     public function index(): Factory|View|Application
     {
         $currentAddress = Address::find($this->cartService->getAddressId());
-
-        return view('cart.index', [
-            'address' => $currentAddress,
-        ]);
+        return view('cart.index', ['address' => $currentAddress]);
     }
 }
