bd046b342c53c0b32fc8dd2d67fd045dbda629ed
bp <bartek.paraf@gmail.com>
2022-10-17 20:52:49 +0200

    moved functionality to CartContainer, cleaned up unused service and variables

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,24 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Address;
-use App\Services\CartService;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 
 class CartController extends Controller
 {
-    private CartService $cartService;
-
-    public function __construct(CartService $cartService)
-    {
-        $this->cartService = $cartService;
-    }
-
     public function index(): Factory|View|Application
     {
-        $currentAddress = Address::find($this->cartService->getAddressId());
-        return view('cart.index', ['address' => $currentAddress]);
+        return view('cart.index');
     }
 }
