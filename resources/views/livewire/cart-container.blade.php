c589ba7b5162e82e8cffb3cd17e75c540b8b6365
bp <bartek.paraf@gmail.com>
2022-10-18 19:07:45 +0200

    added TODO

--- resources/views/livewire/cart-container.blade.php
+++ resources/views/livewire/cart-container.blade.php
@@ -7,6 +7,12 @@
     <hr class="my-4">
     <div class="">
         <h3 class="text-primary mb-4">Bestellobjekte</h3>
+        {{-- TODO how/where to handle the modal?
+                It can be moved here or outside of this component in the Cart itself?
+                Separate all modals to own files?
+                Modals NEED to be livewire components because the data comes from the Backend.. Emit and Catch values?
+        -}}
+        {{--        <x-order-objects-table :orderObjects="$orderObjects" :products="$productsInCart"></x-order-objects-table>--}}
         @livewire('order-object-table')
     </div>
     <hr class="my-4">
