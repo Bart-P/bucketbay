5955dcf8b3f353153d65ed14895edb85a1cd40eb
bp <bartek.paraf@gmail.com>
2022-10-20 19:01:53 +0200

    removed order-objects-table.blade.php, kept the livewire component instead

--- resources/views/livewire/cart-container.blade.php
+++ resources/views/livewire/cart-container.blade.php
@@ -6,13 +6,12 @@
     <hr class="my-4">
     <div class="">
         <h3 class="text-primary mb-4">Bestellobjekte</h3>
-        {{--        <x-order-objects-table :orderObjects="$orderObjects" :products="$productsInCart"></x-order-objects-table>--}}
         @livewire('order-object-table', ['grafics' => $grafics, 'productsInCart' => $productsInCart])
     </div>
     <hr class="my-4">
     <div class="d-flex w-100 justify-content-center">
-        <button class="btn btn-primary btn-lg" type="submit" wire:click="$emit('showSelectGraficsModal')">Bestellung
-            auslösen
+        <button class="btn btn-primary btn-lg" type="submit">
+            Bestellung auslösen
         </button>
     </div>
 </div>
