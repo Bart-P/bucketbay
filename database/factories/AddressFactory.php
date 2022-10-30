127e82866ac922db2ee3938f56ccf6b251cc31a1
bp <bartek.paraf@gmail.com>
2022-10-30 14:36:43 +0100

    removed comment and refactored code

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -10,24 +10,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class AddressFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
+    public function definition(): array
     {
-        return [
-            'name1' => $this->faker->name(),
-            'name2' => $this->faker->text(10),
-            'name3' => $this->faker->text(5),
-            'street' => $this->faker->streetAddress(),
-            'street_nr' => $this->faker->buildingNumber(),
-            'city' => $this->faker->city(),
-            'city_code' => $this->faker->randomNumber(5, true),
-            'country' => $this->faker->country(),
-            'address_info' => $this->faker->text(15),
-            'user_id' => User::all()->random()->id,
-        ];
+        return ['name1'        => $this->faker->name(),
+                'name2'        => $this->faker->text(10),
+                'name3'        => $this->faker->text(5),
+                'street'       => $this->faker->streetAddress(),
+                'street_nr'    => $this->faker->buildingNumber(),
+                'city'         => $this->faker->city(),
+                'city_code'    => $this->faker->randomNumber(5, true),
+                'country'      => $this->faker->country(),
+                'address_info' => $this->faker->text(15),
+                'user_id'      => User::all()->random()->id,];
     }
 }
