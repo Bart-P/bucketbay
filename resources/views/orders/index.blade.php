8833cba3e8f956c5370611845a992d47f3d0fbea
bp <bartek.paraf@gmail.com>
2022-10-30 14:43:31 +0100

    moved order table to its own livewire component.

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,65 +1,9 @@
 <x-layout>
-    <main>
-        <x-banner>
-            <x-slot:heading> Bestellungen </x-slot>
-                Hier eine kurze beschreibung wie die Bestellungen funktioniern..
-        </x-banner>
-        <div class="container">
-            <table class="table table-hover table-responsive">
-                <thead>
-                    <th>Status</th>
-                    <th>ID</th>
-                    <th>Lieferung an</th>
-                    <th>Produkte</th>
-                    <th>Bestellt am</th>
-                    <th>Versendet am</th>
-                    <th></th>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><span class="rounded-pill py-1 px-2 bg-warning text-white">offen</span></td>
-                        <td>1</td>
-                        <td>Test Restaurant</td>
-                        <td>3</td>
-                        <td>10.08.2022 9:05</td>
-                        <td></td>
-                        <td>
-                            <button class="btn btn-outline-primary" style="border: none;"><i
-                                    class="bi-pencil"></i></button>
-                            <button class="btn btn-outline-danger" style="border: none;"><i
-                                    class="bi-trash"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><span class="rounded-pill py-1 px-2 bg-primary text-white">in Bearbeitung</span></td>
-                        <td>2</td>
-                        <td>Test Bar</td>
-                        <td>5</td>
-                        <td>10.08.2022 09:25</td>
-                        <td>Ausstehend</td>
-                        <td>
-                            <button class="btn btn-outline-primary" style="border: none;"><i
-                                    class="bi-pencil"></i></button>
-                            <button class="btn btn-outline-danger" style="border: none;"><i
-                                    class="bi-trash"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td><span class="rounded-pill py-1 px-2 bg-success text-white">Versendet</span></td>
-                        <td>3</td>
-                        <td>Test Club</td>
-                        <td>2</td>
-                        <td>10.08.2022 10:45</td>
-                        <td>11.08.2022 14:05</td>
-                        <td>
-                            <button class="btn btn-outline-primary" style="border: none;"><i
-                                    class="bi-pencil"></i></button>
-                            <button class="btn btn-outline-danger" style="border: none;"><i
-                                    class="bi-trash"></i></button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </main>
+    <x-banner>
+        <x-slot:heading> Bestellungen</x-slot:heading>
+        Hier eine kurze beschreibung wie die Bestellungen funktioniern..
+    </x-banner>
+    <div class="container">
+        @livewire('orders-table')
+    </div>
 </x-layout>
