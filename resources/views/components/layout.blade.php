d2f42c3391b0b7b7228037dfb0a765f9f86aa753
bp <bartek.paraf@gmail.com>
2022-10-30 14:43:07 +0100

    alpinejs src had to be changed, otherwise an error was thrown and view elements where not loaded.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -15,7 +15,6 @@
 
 <body style="min-height: 100vh">
 <header>
-
     @if (!request()->is('*login*'))
         <x-top-navbar></x-top-navbar>
     @endif
@@ -38,7 +37,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
 </script>
-<script src="//unpkg.com/alpinejs" defer></script>
+<script defer src="https://unpkg.com/alpinejs@3.10.5/dist/cdn.min.js"></script>
+
 </body>
 
 </html>
