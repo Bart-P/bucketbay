7a4187ae33903214ba6cb16eb2c0cdb2ef53828a
bp <bartek.paraf@gmail.com>
2022-11-01 11:57:45 +0100

    added relation and db field user_id to orders table

--- database/migrations/2022_10_30_121939_create_orders_table.php
+++ database/migrations/2022_10_30_121939_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->string('status');
             $table->integer('delivery_address_id');
             $table->date('sent_at')->nullable();
