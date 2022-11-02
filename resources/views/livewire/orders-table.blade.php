348b8bd680aaf76f51093b73694e021ed6e0360b
bp <bartek.paraf@gmail.com>
2022-11-02 20:24:39 +0100

    edit and delete is now correctly shown on status open, all orders have additional copy to cart btn

--- resources/views/livewire/orders-table.blade.php
+++ resources/views/livewire/orders-table.blade.php
@@ -11,20 +11,27 @@
     </tr>
     <tbody>
     @if($orders)
-        @foreach($orders as $order)
+        @foreach($orders->sortBy('status') as $order)
             <tr>
                 <td>
-                    <span @class([$this->getPillClass($order->status), 'rounded-pill py-1 px-2'])>{{ $order->status }}</span>
+                    <span @class([$this->getPillClass($order->status), 'rounded-pill py-1 px-2'])>{{ $this->getStatusInDE($order->status) }}</span>
                 </td>
                 <td>{{ $order->id }}</td>
                 <td>{{ $order->name1 }}</td>
                 <td>{{ $order->created_at }}</td>
                 <td>{{ $order->sent_at ? : 'Ausstehend' }}</td>
                 <td class="text-end">
-                    <button class="btn btn-outline-primary" style="border: none;"><i
-                                class="bi-pencil"></i></button>
-                    <button class="btn btn-outline-danger" style="border: none;"><i
-                                class="bi-trash"></i></button>
+                    <button class="btn btn-outline-success" style="border: none;"><i
+                                class="bi-clipboard-plus"></i></button>
+                    @if($this->orderChangePossible($order->status))
+                        <button class="btn btn-outline-primary" style="border: none;"><i
+                                    class="bi-pencil"></i></button>
+                        <button class="btn btn-outline-danger" style="border: none;"><i
+                                    class="bi-trash"></i></button>
+                    @else
+                        <button class="btn btn-outline-primary" style="border: none;"><i
+                                    class="bi-eye-fill"></i></button>
+                    @endif
                 </td>
             </tr>
         @endforeach
