e5056a985f939aacdc0be202c08e885767af03b0
bp <bartek.paraf@gmail.com>
2022-11-02 20:22:29 +0100

    status can now be correctly sorted

--- app/Enums/OrderStatusEnum.php
+++ app/Enums/OrderStatusEnum.php
@@ -4,10 +4,10 @@ namespace App\Enums;
 
 enum OrderStatusEnum: string
 {
-    case OPEN = 'open';
-    case INPROGRESS = 'in_progress';
-    case SENT = 'sent';
-    case INVOICED = 'invoiced';
-    case CLOSED = 'closed';
-    case CANCELLED = 'cancelled';
+    case OPEN = '1 open';
+    case INPROGRESS = '2 in_progress';
+    case SENT = '3 sent';
+    case INVOICED = '4 invoiced';
+    case CLOSED = '5 closed';
+    case CANCELLED = '6 cancelled';
 }
