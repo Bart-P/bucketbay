ef3a1fad885664053287437687aa28d3aa1566d9
bp <bartek.paraf@gmail.com>
2022-11-02 20:03:07 +0100

    removed order status paid

--- app/Enums/OrderStatusEnum.php
+++ app/Enums/OrderStatusEnum.php
@@ -8,7 +8,6 @@ enum OrderStatusEnum: string
     case INPROGRESS = 'in_progress';
     case SENT = 'sent';
     case INVOICED = 'invoiced';
-    case PAID = 'paid';
     case CLOSED = 'closed';
     case CANCELLED = 'cancelled';
 }
