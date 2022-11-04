5f8477cad33d80b9a18eee3a27438f5f9f0e155d
bp <bartek.paraf@gmail.com>
2022-11-04 19:05:12 +0100

    saveOrder() now actualy saves the order to db

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -2,13 +2,21 @@
 
 namespace App\Services;
 
+use App\Enums\OrderStatusEnum;
 use App\Models\Order;
 
 class OrderService
 {
-
-    public function saveOrder(Order $order)
+    public function saveOrder(int $deliveryAddressId, int $printPriceInCent, int $shipmentPriceInCent)
     {
+        $order = new Order;
+
+        $order['user_id'] = auth()->user()->id;
+        $order['delivery_address_id'] = $deliveryAddressId;
+        $order['print_price_in_cent'] = $printPriceInCent;
+        $order['shipment_price_in_cent'] = $shipmentPriceInCent;
+        $order['status'] = OrderStatusEnum::OPEN->value;
+
         $order->save();
     }
 }
